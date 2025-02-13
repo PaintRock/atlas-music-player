2067cc36e53aafa83558c00c0bdb1af82dbbd6fe
Adel Knode <ann.knode@atlasschool.com>
2025-02-13 03:18:42 +0000

    time for change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import MusicPlayer from "./MusicPlayer";
 
 function App() {
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center">
+    <div className="bg-eucalyptus-100 min-h-screen flex flex-col items-center">
       <main className="flex-1 p-8">
       <MusicPlayer />
       </main>
